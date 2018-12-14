78275b8d9eda095da02369a660b27c21e5f85c6c
vhlv <49413456@qq.com>
2018-12-14 16:13:59 +0800

    1

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello,world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date'[:5])
+    output =', '.join([q.question_texxt for q in latest_question_list])
+    # return HttpResponse("Hello,world. You're at the polls index.")
+    return HttpResponse(output)
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
